b24e0c31a4dad71fdb83ed7adfe5ea7cc43ad90b
YALIM GÜRBÜZ <yalim.gurbuz@std.yildiz.edu.tr>
2023-05-23 13:56:34 +0300

    Proje dosyası ekle.

---
+++ JWTWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using JWTWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            user.Username = request.Username;
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            return Ok(user);
+        }
+
+    }
+}
